3f266e4ec32715d2cea7e33336b7f18008ff91fc
acollins1212 <acollins@ucdavis.edu>
2016-10-15 13:34:15 -0700

    Updated scheduler.js
    I'm getting to work on writing the Course and Course_meeting 'classes'

--- scheduler.js
+++ scheduler.js
@@ -7,6 +7,7 @@ function parseSchedule() {
    	var start_time_pattern = /\d{1,2}:\d{2} [A|P]M/;
    	var end_time_pattern = /- (\d{1,2}:\d{2} [A|P]M)/;
    	var days_of_week_pattern = /([A|P]M)([MTWRFS]+)[A-Z]/;
+   	var TEST = new Course(splitSchedule[0]);
 }
 
 
@@ -15,22 +16,25 @@ start_time_pattern = /\d{1,2}:\d{2} [A|P]M/;
 end_time_pattern = /- (\d{1,2}:\d{2} [A|P]M)/;
 days_of_week_pattern = /([A|P]M)([MTWRFS]+)[A-Z]/;
 
-var Course_meeting = function(singleSchedule){
-	this.meeting_type_pattern = /[a-zA-Z]+/gm;
-	this.start_time_pattern = /\d{1,2}:\d{2} [A|P]M/gm;
-	this.end_time_pattern = /- (\d{1,2}:\d{2} [A|P]M)/gm;
-	this.days_of_week_pattern = /([A|P]M)([MTWRFS]+)[A-Z]/gm;
+
+var Course = function(class_string){
+	name_pattern = /(^[A-Z]{3} \d{3}.{0,1}) (.{3}) - (.+)/;
+    schedule_pattern = /'^.*[A|P]M .*/;
+    time_pattern = /.*\d{1,2}:\d{2} [A|P]M/;
+}
+
+
+var Course_meeting = function(single_schedule){
+	meeting_type_pattern = /[a-zA-Z]+/gm;
+	start_time_pattern = /\d{1,2}:\d{2} [A|P]M/gm;
+	end_time_pattern = /- (\d{1,2}:\d{2} [A|P]M)/gm;
+	days_of_week_pattern = /([A|P]M)([MTWRFS]+)[A-Z]/gm;
 
 	this.meeting_type = "";
 	this.start_time = "";
 	this.end_time = "";
 	this.days_of_week = "";
 	this.location = "";
+
 	
-
-
-
-
-
-
 }
