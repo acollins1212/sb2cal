32539867099a661634c2d211afc335e09ebed1dc
acollins1212 <acollins@ucdavis.edu>
2016-10-22 17:22:23 -0700

    Inserting properly, recurrence doesn't work
    Commented out issue #1 (scheduler.js:160)

--- scheduler.js
+++ scheduler.js
@@ -6,8 +6,9 @@ This is translated from python.
 
 **/
 
-var FIRST_DAY = new Date(2017, 0, 9, 1, 0, 0, 0);
-var LAST_DAY = new Date(2017, 2, 18, 16, 59, 0, 0);
+
+var  FIRST_DAY = new Date(2017, 0, 9, 1, 0, 0, 0);
+var  LAST_DAY = new Date(2017, 2, 18, 16, 59, 0, 0);
 
 function parseTime(time_str) {
 
@@ -52,57 +53,56 @@ function parseSchedule() {
    	//TEST.getEventJSON(0);
 
    	return split_schedule;
-}
+   }
 
-var Course = function(class_string){
-	
+   var Course = function(class_string){
+
 	//WEEKLY SCHEDULE PORTION
 	var name_pattern = /(^[A-Z]{3} \d{3}[A-Z]{0,2}) (.{3}) - (.+)/m;
-    var schedule_pattern = /^.*[A|P]M .*/gm;
-    var exam_pattern = /.* (\d{1,2}\/\d{1,2}\/\d{4}) (\d{1,2}:\d{2} [A|P]M)/m;
+	var schedule_pattern = /^.*[A|P]M .*/gm;
+	var exam_pattern = /.* (\d{1,2}\/\d{1,2}\/\d{4}) (\d{1,2}:\d{2} [A|P]M)/m;
 	
 
-    var reg_obj_name = class_string.match(name_pattern);
+	var reg_obj_name = class_string.match(name_pattern);
 
-    this.name_str = reg_obj_name[1];
-    this.section = reg_obj_name[2];
-    this.description_str = reg_obj_name[3];
+	this.name_str = reg_obj_name[1];
+	this.section = reg_obj_name[2];
+	this.description_str = reg_obj_name[3];
 
-    this.schedule_array = class_string.match(schedule_pattern);
-    this.meeting_array = [];
-    
-    
-    for(i = 0; i < this.schedule_array.length; i++){
-    	this.meeting_array.push(new Course_meeting(this.schedule_array[i]));
-    }
+	this.schedule_array = class_string.match(schedule_pattern);
+	this.meeting_array = [];
 
-    //console.log(this.meeting_array);
+
+	for(i = 0; i < this.schedule_array.length; i++){
+		this.meeting_array.push(new Course_meeting(this.schedule_array[i]));
+	}
 
 	
 	//FINAL EXAM PORTION
-    this.finalExam_str = class_string.match(exam_pattern);
+	this.finalExam_str = class_string.match(exam_pattern);
 	//The date is in MM/DD/YYYY format. 
-    var mmddyyyy = this.finalExam_str[1].split('/');
+	var mmddyyyy = this.finalExam_str[1].split('/');
 	var time = parseTime(this.finalExam_str[2]);
 	//Months are zero indexed, so I'm subtracting one inside constructor
 	this.finalExam_date = new Date(mmddyyyy[2], mmddyyyy[0] - 1, mmddyyyy[1], time[0], time[1], 0, 0 );
-	//console.log(this.finalExam_date.toISOString()); //FIXME???
 	
 	
-    this.getEventJSON = function(meeting_number) {
-    	//course_code ex: "ABC 101"
-    	//course_meeting  can be lecture, discussion, lab, etc
+	this.getEventJSON = function(meeting_number) {
 
-    	var course_event = this.meeting_array[meeting_number];
-    	var desc = this.description_str + "\nSection: " + this.section;
+		var course_event = this.meeting_array[meeting_number];
+		var desc = this.description_str + "\nSection: " + this.section;
 		
-		var frequency_string = 'FREQ=WEEKLY';
-		var until_string = 'UNTIL='+LAST_DAY.toISOString().replace('.000Z', '-07:00');
-		var byday_string = "BYDAY="+course_event.days_of_week;
-		var recurrence_string = 'RRULE:WKST=SU;'+frequency_string+';'+until_string+';'+byday_string;
-		console.log(recurrence_string);
+		var last_day_iso = LAST_DAY.toISOString().replace(/[:\-.]/g,'');
+		
 
-		console.log(meeting_number);
+		//RECURRENCE STRING
+		var frequency_string = 'FREQ=WEEKLY';
+		var until_string = 'UNTIL='+last_day_iso;
+		var byday_string = "BYDAY="+course_event.days_of_week;
+		var recurrence_string = 'RRULE:WKST=SU;' + frequency_string + ';'
+		+ until_string + ';' +byday_string;
+		console.log(until_string);
+
 		//To give the proper start date of a class
 		var addition = 0;
 		if(course_event.days_of_week.search('MO') != -1){
@@ -122,7 +122,7 @@ var Course = function(class_string){
 			addition = 4;
 		}
 
-		var start_datetime = FIRST_DAY;
+		var start_datetime = new Date(FIRST_DAY);
 		start_datetime.setDate(FIRST_DAY.getDate() + addition);
 		start_datetime.setHours(course_event.start_time[0] - 7); //-7 accounts for toISOString() adding 7
 		start_datetime.setMinutes(course_event.start_time[1]);
@@ -131,12 +131,17 @@ var Course = function(class_string){
 
 		console.log(start_datetime);
 
-		var end_datetime = FIRST_DAY;
-		end_datetime.setHours(course_event.end_time[0] - 7); //-7 accounts for toISOString() adding 7
+		var end_datetime = new Date(FIRST_DAY);
+		end_datetime.setDate(FIRST_DAY.getDate() + addition);
+		end_datetime.setHours(course_event.end_time[0] - 7); //-7 accounts for toISOString() adding 7 
 		end_datetime.setMinutes(course_event.end_time[1]);
 		end_datetime = end_datetime.toISOString();
 		end_datetime = end_datetime.replace('.000Z', '-07:00');
+
 		console.log(end_datetime);
+		
+		//I'M REALLY NOT UNDERSTANDING TIMEZONING. HOPEFULLY ILL UNDERSTAND IT L8R
+		//I JUST DISCOVERED THAT THE GCALENDAR IS SET TO GMT (due to daylight savings)
 
 		var summary_string = this.name_str + " " + course_event.meeting_type;
 		
@@ -155,13 +160,21 @@ var Course = function(class_string){
 			//"recurrence": [
 			//	recurrence_string
 			//]
-			//"reminders": {
-			//	"useDefault": false
+
+			/*
+			"reminders": {
+				"useDefault": false;
+				'overrides': [
+				{'method': 'email', 'minutes': 24 * 60},
+				{'method': 'popup', 'minutes': 10}
+				]
+
 				//FEATURE IDEA: Ask user whether they want reminders
-			//}
+			}
+			*/
+
 			//FEATURE IDEA: Ask user what color they want for the calendar
 		}; //event
-
 		return event;
 
     } //this.getEventJSON()
@@ -200,7 +213,6 @@ var Course_meeting = function(single_schedule){
 	//Taking end_time out of schedule passed in
 	if (this.end_time = single_schedule.match(end_time_pattern)[1]) {
 		this.end_time = parseTime(this.end_time);
-
 	} //do nothing
 	else {
 		console.log("end_time null");
@@ -211,13 +223,13 @@ var Course_meeting = function(single_schedule){
 
 		//RFC 5545 takes recurrence dates given with two letter abbreviations
 		this.days_of_week = this.days_of_week.replace('M', 'MO,');
-        this.days_of_week = this.days_of_week.replace('T', 'TU,');
-        this.days_of_week = this.days_of_week.replace('W', 'WE,');
-        this.days_of_week = this.days_of_week.replace('R', 'TH,');
-        this.days_of_week = this.days_of_week.replace('F', 'FR,');
-   	    var dow_length = this.days_of_week.length-1;
+		this.days_of_week = this.days_of_week.replace('T', 'TU,');
+		this.days_of_week = this.days_of_week.replace('W', 'WE,');
+		this.days_of_week = this.days_of_week.replace('R', 'TH,');
+		this.days_of_week = this.days_of_week.replace('F', 'FR,');
+		var dow_length = this.days_of_week.length-1;
 
-        this.days_of_week = this.days_of_week.substring(0,dow_length);
+		this.days_of_week = this.days_of_week.substring(0,dow_length);
 	}
 	else {
 		console.log("dow null");
