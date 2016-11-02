5e288c04a6f5241ecfb873b105d5e1afb69911b0
A.J. Collins <acollins@ucdavis.edu>
2016-11-01 17:46:21 -0700

    Worked a little bit on style
    I want to come up with a minimalist design with UCD colors

--- main.php
+++ main.php
@@ -1,36 +1,45 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" type="text/css" href="./mainstyle.css">
+  <link rel="stylesheet" type="text/css" href="mainstyle.css">
   
   <script type="text/javascript" src="scheduler.js"></script>
   <script type="text/javascript" src="ID+scope.js">
     //NOT UPLOADED TO GITHUB FOR PRIVACY REASONS 
-    //contains vital variables: CLIENT_ID, SCOPES, CALENDAR_ID
+    //contains vital variables: CLIENT_ID and SCOPES
   </script> 
   <script src="https://apis.google.com/js/client.js?onload=checkAuth"></script>
   </head>
 
   <body>
+	<div id="main-container">
     <h1>Schedule Builder to Google Calendar</h1>
-    <p>Instructions</p>
-	<ol>
-		<li>Create a new calendar in Google Calendar. Take note of its calendar id. This can be done by going to the settings of the calendar and looking near the bottom</li>
-		<li>Go to <a href="https://my.ucdavis.edu/schedulebuilder/index.cfm?sb" target="_blank">Schedule Builder</a></li>
-		<li>Click "Show (Important) Details" for every single course in your schedule</li>
-		<li>Highlight all of your schedules on the page.</li>
-		<li>Paste your schedule into the text box below</li>
-    <li>Paste your calendar id in the second text box</li>
-		<li>Click Submit. The next page will give you a chance to edit any of your schedule entries</li>
-		<li>Insert into Google Calendar!</li>
-	</ol>
+	<div id="instruction-div">
+		<p><strong>Instructions</strong></p>
+		<ol>
+			<li>Create a new calendar in Google Calendar.</li>
+				<ul>
+					<li> Take note of its Calendar Id</li>
+					<li>"Calendar Settings" → "Calendar Address"</li>
+				</ul>
+			<li>Go to <a href="https://my.ucdavis.edu/schedulebuilder/index.cfm?sb" target="_blank">Schedule Builder</a></li>
+			<li>Click "Show (Important) Details" for every single course in your schedule</li>
+			<li>Highlight all of your schedules on the page.</li>
+			<li>Paste your schedule into the text box below</li>
+			<li>Paste your calendar id in the second text box</li>
+			<li>Click Submit. The next page will give you a chance to edit any of your schedule entries</li>
+			<li>Insert into Google Calendar!</li>
+		</ol>
+	</div>
     <div>
       <form action="handle.php" method="post">
 
-        <textarea name="textArea" id="textArea_id" placeholder="Paste schedule here" ></textarea>
-        <div><input name="CALENDAR_ID" id="CALENDAR_ID" type="text"  placeholder="Insert Calendar ID"></div>
+        <div><input name="CALENDAR_ID" id="calendar-id" type="text"  placeholder="xxxxxxxxxx@group.calendar.google.com"></div>
+        <textarea name="textArea" id="textArea-id" placeholder="Paste schedule here" ></textarea>
  
 <!-- 
+		DON'T FORGET ABOUT THIS FUNCTIONALITY!
+		
         <input type="radio" name="reminders" value="none" checked>None<br>
         <input type="radio" name="reminders" value="email">Email<br>
         <input type="radio" name="reminders" value="popup">Popup<br>
@@ -45,9 +54,11 @@
 
     
     <pre id="output"></pre>
+	<br><br><br>
+	<p style="text-align: right"> sb2cal.com </p>
 
     
 
-    
+	</div>
   </body>
   </html>
